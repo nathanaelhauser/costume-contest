b164e5fdebc6d47e5f95f821337205d0fb34b8cb
Nathanael Hauser <nathanaelhauser@gmail.com>
2019-10-31 19:05:10 -0700

    blah

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
 const inquirer = require('inquirer')
 const mysql = require('mysql2')
 
+// const db = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'password',
+//   database: 'costumes_db'
+// })
+
+const votingMenu = _ => {
+  inquirer.prompt({
+    type: 'input',
+    name: 'voter',
+    message: 'What is your name?'
+  })
+    .then(answer => {
+      console.log(hello)
+      console.log(answer)
+    })
+    .catch(e => console.log(e))
+}
+
+votingMenu()
