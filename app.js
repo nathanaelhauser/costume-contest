8fbcc5738969868c372eab5ea6cfae090e5bbfe3
Mercedes Walsh <sadieswalsh@gmail.com>
2019-10-31 19:12:02 -0700

    added leaderboard function

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 const inquirer = require('inquirer')
 const mysql = require('mysql2')
 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: costumes_db
+});
+
+function leaderboard () {
+    connection.query('SELECT * FROM contestants ORDER BY score', (e, r, fields) => {
+        if (e) {
+            console.log(e)
+        }
+        console.log(r)
+        process.exit()
+    })
+}
